030d435a25ede69532694243313326a80d881bcd
LuisGildardoAlvarado <luisgil9900@gmail.com>
2025-03-28 21:12:36 -0700

    First commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p>I try to make something form me</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="bg-red-400 rounded-md">
+      <h1>Dashboard</h1>
+      <p>Welcome to your dashboard.</p>
+    </div>
+  );
+}
